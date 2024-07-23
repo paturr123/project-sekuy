8f7ba403d375351b12bb887708732a85447a917f
paturr123 <khafialalf@gmail.com>
2024-07-23 09:47:56 +0800

    menambahkan table

--- crud-project/resources/views/index.blade.php
+++ crud-project/resources/views/index.blade.php
@@ -8,75 +8,89 @@
 
           {{-- columns --}}
           <div class="container" style="margin-right: 183px;">
-                    <div class="row">
-                      <div class="col-2">
+              <div class="row">
+                <div class="col-2">
                         
-                      {{-- side bar --}}
-                    <div class="d-flex flex-column flex-shrink-0 p-3 bg-light" style="width: 290px; height:610px; outline:1px solid black;">
-                    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-                      <svg class="bi me-2" width="10" height="32"><use xlink:href="#bootstrap"></use></svg>
-                      <span class="fs-3 fw-bold">|MENU UTAMA|</span>
+                  {{-- side bar --}}
+                  <div class="d-flex flex-column flex-shrink-0 p-3 bg-light" style="width: 290px; height:610px; outline:1px solid black;">
+                  <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+                    <svg class="bi me-2" width="10" height="32"><use xlink:href="#bootstrap"></use></svg>
+                    <span class="fs-3 fw-bold">|MENU UTAMA|</span>
+                  </a>                  
+
+                  <hr style="height: 3px;">                    
+                  <ul class="nav flex-column mb-auto">
+                  <li class="nav-item mt-4" style="outline:1px solid black; border-radius: 0.3rem;">
+                    <a href="#" class="nav-link link-warning" aria-current="page" type="button">
+                    <svg class="bi me-2 " width="16" height="16"><use xlink:href="#home"></use></svg>
+                        Kelas                        
                     </a>
+                  </li>
 
-                    <hr style="height: 3px;">                    
-                    <ul class="nav nav-pills flex-column mb-auto">
-                    <li class="nav-item">
-                        <a href="#" class="nav-link active" aria-current="page">
-                          <svg class="bi me-2" width="16" height="16"><use xlink:href="#home"></use></svg>
-                          Kelas
-                        </a>
-                    </li>
-
-                    <li>
-                        <a href="#" class="nav-link link-dark">
-                          <svg class="bi me-2" width="16" height="16"><use xlink:href="#speedometer2"></use></svg>
-                          Murid
-
-                        </a>
-
-                    </li>
-
-                    <li>
-                        <a href="#" class="nav-link link-dark">
-                          <svg class="bi me-2" width="16" height="16"><use xlink:href="#table"></use></svg>
-                          Mapel
-                        </a>
-                    </li>                    
-                    
-                    {{-- bottom --}}
-                    <hr style="height: 18px;">
-                    <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br>
-
-                    <div class="dropdown">
-                      <a href="#" class="d-flex align-items-center link-dark text-decoration-none dropdown-toggle" id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
-
-                        <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
-                        <strong>mdo</strong>
-                      </a>
-                      
-                    <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser2">
-                              <li><a class="dropdown-item" href="#">New project...</a></li>
-                              <li><a class="dropdown-item" href="#">Settings</a></li>
-                              <li><a class="dropdown-item" href="#">Profile</a></li>
-                              <li><hr class="dropdown-divider"></li>
-                              <li><a class="dropdown-item" href="#">Sign out</a></li>
-                              </ul>
-                              </div>
-                    </div>
-                    </div>
-                    <div class="col-8" style="margin-left: 91px;">
-
-                    {{-- container --}}
-                    <div class="flex-column flex-shrink-0 p-3 bg-light" style=" width: 1001px; height:74px; margin-left: 10px; outline:1px solid black;">
-                    <a class="align-items-right mb-3 me-md-auto link-dark text-decoration-none">
-                      <svg style="margin-top: 3px;" class="bi me-3" width="10" height="32"><use xlink:href="#bootstrap"></use></svg>
-                      <span class="fs-4" style="margin-top: 8px;">KELAS</span>                
-                      
-                      <button type="button" class="btn btn-primary" style="margin-left: 730px; margin-bottom: 10px;">Logout</button>
+                  <li class="nav-item mt-2" style="outline:1px solid black; border-radius: 0.3rem;">
+                    <a href="#" class="nav-link link-dark">
+                      <svg class="bi me-2" width="16" height="16"><use xlink:href="#speedometer2"></use></svg>
+                        Murid
                     </a>
-                    </div>
-                    </div>                    
+                  </li>
+
+                  <li class="nav-item mt-2 mb-3" style="outline:1px solid black; border-radius: 0.3rem;">
+                    <a href="#" class="nav-link link-dark">
+                      <svg class="bi me-2" width="16" height="16"><use xlink:href="#table"></use></svg>
+                        Mata Pelajaran
+                    </a>
+                  </li>                    
+                     
+                  {{-- bottom --}}
+                  <hr style="height: 18px;">
+                  <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> 
+
+                  <div class="dropdown">
+                    <a href="#" class="d-flex align-items-center link-dark text-decoration-none dropdown-toggle" id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
+
+                    <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
+                      <strong>mdo</strong>
+                    </a>
+                      
+                  <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser2">
+                      <li><a class="dropdown-item" href="#">New project...</a></li>
+                      <li><a class="dropdown-item" href="#">Settings</a></li>
+                      <li><a class="dropdown-item" href="#">Profile</a></li>
+                      <li><hr class="dropdown-divider"></li>
+                      <li><a class="dropdown-item" href="#">Sign out</a></li>
+                  </ul>
+                  </div>
+
+                  </div>
+
+                  </div>
+                  
+
+                  <div class="col-8" style="margin-left: 91px;">
+
+                  {{-- container --}}                  
+                  <div class="flex-column flex-shrink-0 p-3 bg-light" style=" width: 1001px; height:74px; margin-left: 10px; outline:1px solid black;">
+                  <a class="align-items-right mb-3 me-md-auto link-dark text-decoration-none">
+                  <svg style="margin-top: 3px;" class="bi me-3" width="10" height="32"><use xlink:href="#bootstrap"></use></svg>
+                    <span class="fs-4" style="margin-top: 8px;">KELAS</span>
+                      
+                    <button type="button" class="btn" style="margin-left: 730px; margin-bottom: 10px; outline:1px solid black;">Logout</button>
+                  </a>
+                  </div>
+                </div>                    
           </div>
+
+          
+          {{-- table --}}
+          <div class="card">
+            <h5 class="card-header">Kelas</h5>
+            <div class="card-body">
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+                
 
 
                    
